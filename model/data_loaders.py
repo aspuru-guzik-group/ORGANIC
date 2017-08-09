651f96a2df277b8a11f4c68ff6ec6dfac6215521
Carlos Outeiral <couteiral@gmail.com>
2017-08-09 13:30:26 -0400

    Deleted old function split_unicode_chrs

--- model/data_loaders.py
+++ model/data_loaders.py
@@ -37,12 +37,6 @@ class Gen_Dataloader():
         self.pointer = 0
 
 
-def split_unicode_chrs(text):
-    _unicode_chr_splitter = _Re(
-        '(?s)((?:[\ud800-\udbff][\udc00-\udfff])|.)').split
-    return [chr for chr in _unicode_chr_splitter(text) if chr]
-
-
 class Dis_Dataloader():
     def __init__(self):
         self.vocab_size = 5000
